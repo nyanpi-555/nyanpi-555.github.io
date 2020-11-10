d6d741f4958872fe3f41501e224fa8e6b02173d0
nyanpi-555 <50684606+nyanpi-555@users.noreply.github.com>
2020-11-10 18:43:17 +0900

    yay

---
+++ project_1_coffeeroaster/js/main.js
@@ -0,0 +1,73 @@
+$(function () {
+
+  const $win = $(window);
+
+  // スマホメニュー
+  (function () {
+    const $btnMenu = $('.btn-menu');
+    const $nav = $('header > nav');
+    const isAnimate = 'is-animate';
+    const isOpen = 'is-open';
+
+    // ボタンクリック時の処理
+    $btnMenu.on('click', function () {
+      const $this = $(this);
+      if ($nav.is(':animated')) {
+        return;
+      }
+      if (!$this.hasClass(isOpen)) {
+        $nav.slideDown();
+        $this.addClass(isOpen).addClass(isAnimate);
+      } else {
+        $nav.slideUp();
+        $this.removeClass(isOpen).removeClass(isAnimate);
+      }
+    });
+
+    // ウィンドウリサイズ時の処理
+    $win.on('resize', function () {
+      if ($win.width() > 768) {
+        $nav.show();
+        $btnMenu.removeClass(isOpen).removeClass(isAnimate);
+      } else {
+        if (!$btnMenu.hasClass(isOpen)) {
+          $nav.hide();
+        }
+      }
+    });
+  })();
+
+  // 見出しのアニメーション
+  (function () {
+    $win.on('scroll', function () {
+      const $heading = $('.mask_inner');
+      const isAnimate = 'is-animate';
+      $heading.each(function () {
+        const $this = $(this);
+        const headingTop = $this.offset().top;
+        const scrollPos = $win.scrollTop();
+        const wh = $win.height();
+        if (scrollPos > headingTop - wh + (wh / 4)) {
+          $this.addClass(isAnimate);
+        }
+      });
+    });
+  })();
+
+  // スムーススクロール
+  (function () {
+
+    const $body = $('body, html');
+    const duration = 1000;
+    const motion = 'swing';
+
+    $('a[href^="#"]').on('click', function () {
+      const $this = $(this);
+      const href = $this.attr('href');
+      const target = href === '#top' ? 'body' : href;
+      const position = $(target).offset().top;
+      $body.animate({ scrollTop: position }, duration, motion);
+    });
+
+  })();
+});
